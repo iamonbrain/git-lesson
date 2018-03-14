516a1fcf6e6b8819f8e9725cba52e5d191b3338b
iamonbrain@gmail.com <iamonbrain@gmail.com>
2018-03-14 21:25:02 +0700

    Добавить алерт на кнопку

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by iamon on 14.03.2018.
+ */
+document.addEventListener('DomContetntLoaded', function (ev) {
+    document.querySelector('#btn').addEventListener('click', function (ev) {
+        alert('Привет мир!')
+    })
+})
